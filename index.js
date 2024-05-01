781b855ee2b4fb0f6323615cd88d560309a8335c
Burlacu Rodion <burlacurodionmd@gmail.com>
2024-05-01 21:30:14 +0300

    add first ex

---
+++ index.js
@@ -0,0 +1,37 @@
+// fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+//       .then(response => response.json())
+//       .then((data) => console.log(data))
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts',{
+//     method:'POST',
+//     body:JSON.stringify({
+//         name: 'some name'
+//     }),
+//     headers: {
+//         'Content-type':'application/json; charset=UTF-8',
+//     },
+// })
+// .then(response => response.json())
+// .then((data) => console.log(data))
+
+// fetch('https://jsonplaceholder.typicode.com/posts/77',{
+//     method:'PUT',
+//     body:JSON.stringify({
+//         name: 'some name'
+//     }),
+//     headers: {
+//         'Content-type':'application/json; charset=UTF-8',
+//     },
+// })
+// .then(response => response.json())
+// .then((data) => console.log(data))
+
+// fetch('https://jsonplaceholder.typicode.com/delete/77',{
+//     method:'DELETE',
+// })
+// .then(response => response.json())
+// .then((data) => console.log(data))
+
+
+
