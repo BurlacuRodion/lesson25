c3fdc5e57fe689d8150e5fe903fcdc8ffd6e089b
Burlacu Rodion <burlacurodionmd@gmail.com>
2024-05-01 22:41:12 +0300

    add ex

--- index.js
+++ index.js
@@ -33,5 +33,55 @@
 // .then(response => response.json())
 // .then((data) => console.log(data))
 
+//-------------------------------------------------------------------//
+
+const root = document.getElementById('root');
+
+const users = {
+    1:'Mark',
+    2:'Alex',
+    3:'John',
+    4:'Steve',
+    5:'Mike',
+    6:'Ana',
+    7:'Lena',
+    8:'Donna',
+    9:'Alexia',
+    10:'Milla'
+}
+
+function addUserList(list,name) {
+    const wrapper = document.createElement('div');
+    const postList = document.createElement('ol');
+    const title = document.createElement('p');
+
+    title.innerText = `${name}'s posts:`;
+    title.style.paddingLeft = '25px';
+    title.style.fontWeight = '700';
+
+    list.forEach((post) => {
+        const li = document.createElement('li')
+        li.innerText = post.title
+        postList.appendChild(li)
+    });
+    wrapper.appendChild(title)
+    wrapper.appendChild(postList)
+    wrapper.style.width = '30%'
+    root.appendChild(wrapper)
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then((data) => {
+    for (let key in users) {
+        let list = data.filter((list)=>{
+            return Number(key) === list.userId
+        })
+        addUserList(list,users[key])
+    }
+})
+
+
+
 
 
